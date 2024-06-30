df4716725eaed7249b2c1ee4f0d0f13f711ec2f6
Rodrigo Santana dos Santos <ssantos.rodrigo89@gmail.com>
2024-06-30 15:35:13 -0300

    Adicionado endpoint HTML e testes correspondentes

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,3 +14,12 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK  # Assert (afirmar)
     assert response.json() == {'message': 'Olá Mundo!'}  # Assert (afirmar)
+
+
+def test_read_root_html_deve_retornar_ok_e_ola_mundo_html():
+    client = TestClient(app)  # Arrange (organização)
+
+    response = client.get('/html')  # Act (ação)
+
+    assert response.status_code == HTTPStatus.OK  # Assert (afirmar)
+    assert response.text == '<html><body><h1>Olá Mundo!</h1></body></html>'
